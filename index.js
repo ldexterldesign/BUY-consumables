d3e7b2bfff9911ca7710032d04fe2089599b3605
Lewis Dexter Litanzios <ldexterldesign@Lewiss-MacBook-Pro.local>
2022-06-29 22:32:50 +0100

    JS Revoke & delete API key

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let apikey = "CELUISDw1fIjjwhIywZcQqYqVE1lfOa9m6JC5HXK";
+let apikey = null
 let food = "175140"; // sardine
 let url = "https://api.nal.usda.gov/fdc/v1/food";
 const jsonResponse = await fetch(`${url}/${food}?api_key=${apikey}`);
